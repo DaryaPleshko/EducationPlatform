cea4fbe05d7e515e7dc56f2d7aba7c6151c62bd4
Darya Pleshko <daryapleshko.work.dev@mail.ru>
2023-08-10 22:13:35 +0300

    commit

---
+++ client/src/Pages/HomePage/HomePage.js
@@ -0,0 +1,14 @@
+import Header from "../../components/Header/Header";
+import Preview from "../../components/Preview/Preview";
+
+function HomePage() {
+    return (
+        <div>
+            <Header />
+            <Preview/>
+        </div>
+    );
+}
+
+export default HomePage;
+
