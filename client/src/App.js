66ef478a6401a20129a2578a17b95eb771be5d23
Darya Pleshko <daryapleshko.work.dev@mail.ru>
2023-08-16 22:13:16 +0300

    commit

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,16 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import HomePage from './Pages/HomePage/HomePage';
+import RegPage from './Pages/RegPage/RegPage';
+import AuthPage from './Pages/AuthPage/AuthPage';
 
 function App() {
   return (
-    <div>
-      <HomePage />
-    </div>
+    <Routes>
+      <Route path='/' element={<HomePage />}></Route>
+      <Route path='/reg' element={<RegPage />}></Route>
+      <Route path='/auth' element={<AuthPage />}></Route>
+    </Routes>
   );
 }
 
---
+++ client/src/Pages/AuthPage/AuthPage.js
@@ -0,0 +1,13 @@
+import Footer from "../../components/Footer/Footer";
+import Header from "../../components/Header/Header";
+
+function AuthPage() {
+    return (
+        <div>      
+            <Header/>
+            <Footer/>
+        </div>
+    );
+}
+
+export default AuthPage;
